123c62dd7851027557dc30839ae4f0cd36350433
TAskMAster339 <145381480+TAskMAster339@users.noreply.github.com>
2023-09-21 15:55:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+A = []
+
+
+@app.get("/")
+def hello():
+    return {"Calculator": "v1"}
+
+
+@app.get("/calc/result")
+def get_calculation():
+    """Выводит последний результат"""
+    return A[-1] if len(A) > 0 else None
+
+
+@app.delete("/calc/clear")
+def clear_list():
+    """Очищает список с результатами"""
+    A.clear()
+
+
+@app.get("/calc/history")
+def get_history():
+    """Выводит историю калькулятора"""
+    return A
+
+
+@app.post("/calc")
+def make_calculations(a: int, b: int, char: str):
+    """a - первое чиcло,
+       b - второе число,
+       char - знак мат. операции
+       ( + - * / )
+    """
+    if char == '+':
+        A.append(a + b)
+        return get_calculation()
+    elif char == '-':
+        A.append(a - b)
+        return get_calculation()
+    elif char == '*':
+        A.append(a * b)
+        return get_calculation()
+    elif char == '/':
+        A.append(a / b)
+        return get_calculation()
+    else:
+        return {"Mistake": "404"}
